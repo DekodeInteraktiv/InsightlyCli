e562a40b593e250b2f9ff90ab8960f31473de2e4
Rasmus Rimestad <rasmus@dekode.no>
2019-04-01 10:04:04 +0200

    Added list-db-servers command

---
+++ app/includes/commands/class-list-db-servers.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Dekode\InsightlyCli\Commands;
+
+use Dekode\InsightlyCli\Services\InsightlyService;
+use Dekode\InsightlyCli\Services\OperatingSystemService;
+use Dekode\InsightlyCli\Services\SSHService;
+
+class ListDbServers extends Command {
+
+	/**
+	 * Returns the string used to run this command.
+	 *
+	 * @return string
+	 */
+	public function get_key(): string {
+		return 'list-db-servers';
+	}
+
+	/**
+	 * Returns a short description for this command-
+	 *
+	 * @return string
+	 */
+	public function get_description(): string {
+		return 'Tries to find the DB host of all sites';
+	}
+
+	/**
+	 * Returns the help text for this command.
+	 *
+	 * @return string
+	 */
+	public function get_help(): string {
+		$help = "Usage:\nisc " . $this->get_key() . "\n\n";
+
+		return $help;
+
+	}
+
+	/**
+	 * Executes this command
+	 */
+	public function run() {
+		$insightly_service = new InsightlyService( INSIGHTLY_API_KEY );
+		$projects          = $insightly_service->get_projects();
+
+		usort( $projects, function ( $a, $b ) {
+			return strcmp( strtolower( $a->get_name() ), strtolower( $b->get_name() ) );
+		} );
+
+
+		foreach ( $projects as $project ) {
+			$db_details = [];
+
+			try {
+				@$ssh_service = new SSHService( $project );
+
+				@$db_details = $ssh_service->get_db_details();
+			} catch (\Exception $e) {
+				$db_details['DB_HOST'] = 'Error: ' . $e->getMessage();
+			}
+
+			if (!$db_details['DB_HOST']) {
+				$db_details['DB_HOST'] = 'Unknown or localhost';
+			}
+
+			print($project->get_name() . ': ' . $db_details['DB_HOST'] . "\n");
+		}
+
+
+	}
+}
--- app/insightly-cli.php
+++ app/insightly-cli.php
@@ -1,6 +1,6 @@
 <?php
 
-define( 'APP_VERSION', '1.11.0' );
+define( 'APP_VERSION', '1.12.0' );
 
 require( 'vendor/autoload.php' );
 require( 'config.php' );
@@ -45,6 +45,7 @@ require( __DIR__ . '/includes/commands/class-sanity-check.php' );
 require( __DIR__ . '/includes/commands/class-ssh.php' );
 require( __DIR__ . '/includes/commands/class-update.php' );
 require( __DIR__ . '/includes/commands/class-users.php' );
+require( __DIR__ . '/includes/commands/class-list-db-servers.php' );
 
 
 $core = new \Dekode\InsightlyCli\Core( [
@@ -62,7 +63,8 @@ $core = new \Dekode\InsightlyCli\Core( [
 	new \Dekode\InsightlyCli\Commands\DekodemonSanityCheck(),
 	new \Dekode\InsightlyCli\Commands\Users(),
 	new \Dekode\InsightlyCli\Commands\CheckLoad(),
-	new \Dekode\InsightlyCli\Commands\Ls()
+	new \Dekode\InsightlyCli\Commands\Ls(),
+	new \Dekode\InsightlyCli\Commands\ListDbServers()
 ] );
 
 if ( $argv[0] == 'php' ) {
